b9525ccc3b91ff0b973da87bb1f70f58fbd42aa6
Thomas Liu <s192287@student.hb.se>
2021-09-01 01:19:34 +0200

    Add new controller.

---
+++ DemoTest.Api/Controllers/CustomersController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DemoTest.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomersController : ControllerBase
+    {
+        private readonly CustomerContext context;
+
+        public CustomersController(CustomerContext context)
+        {
+            this.context = context;
+        }
+
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(IEnumerable<Customer>))]
+        public async Task<IActionResult> GetAll() => Ok(await context.Customers.ToArrayAsync());
+
+        [HttpGet]
+        [Route("{id}")]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(Customer))]
+        public async Task<IActionResult> GetById(int id)
+        {
+            var existingCustomer = await context.Customers.FirstOrDefaultAsync(x => x.Id == id);
+            if (existingCustomer == null) return NotFound();
+            return Ok(existingCustomer);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Add([FromBody] Customer customer)
+        {
+            await context.Customers.AddAsync(customer);
+            await context.SaveChangesAsync();
+            return CreatedAtAction(nameof(GetById), new { id = customer.Id }, customer);
+        }
+    }
+}
