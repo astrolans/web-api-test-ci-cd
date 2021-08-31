5623657c139a261910a4b2feae97ad52f41c314f
Thomas Liu <s192287@student.hb.se>
2021-08-31 17:42:39 +0200

    Add code.

---
+++ DemoTest.Tests/DemoTest.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace DemoTest.Tests
+{
+    public class DemoTest
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.True(1 == 1);
+        }
+    }
+}
