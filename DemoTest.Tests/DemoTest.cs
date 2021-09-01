0462a0c9168b9a76f6e6b9534f11ee5c8a25a6ce
Thomas Liu <s192287@student.hb.se>
2021-09-01 02:04:05 +0200

    Add integration test.

--- DemoTest.Tests/DemoTest.cs
+++ DemoTest.Tests/DemoTest.cs
@@ -1,4 +1,12 @@
+using DemoTest.Api;
+using DemoTest.Api.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using Xunit;
 
 namespace DemoTest.Tests
@@ -9,6 +17,36 @@ namespace DemoTest.Tests
         public void Test1()
         {
             Assert.True(1 == 1);
+        }
+
+        [Fact]
+        public async Task CustomerIntegrationTest()
+        {
+            // Create DB
+            var configuration = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json")
+                //.AddEnvironmentVariables()
+                .Build();
+            var optionsBuilder = new DbContextOptionsBuilder<CustomerContext>();
+            optionsBuilder.UseSqlServer(configuration["ConnectionStrings:DefaultConnection"]);
+            var context = new CustomerContext(optionsBuilder.Options);
+
+            // Deleta all existing Customers in DB (or drop DB)
+            await context.Database.EnsureDeletedAsync();
+            await context.Database.EnsureCreatedAsync();
+
+            // Create Controller
+            var controller = new CustomersController(context);
+
+            // Add Customer
+            await controller.Add(new Customer { CustomerName = "Fox Mulder" });
+
+            // Check: Does GetAll return the added Customer?
+            //var orgRes = await controller.GetAll();
+            var actionResult = (OkObjectResult)await controller.GetAll();
+            var result = ((IEnumerable<Customer>)actionResult.Value).ToArray();
+            Assert.Single(result);
+            Assert.Equal("Fox Mulder", result[0].CustomerName);
         }
     }
 }
